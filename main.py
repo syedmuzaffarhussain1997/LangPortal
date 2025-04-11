1b649e6ac45bd7462583de26fc25944ae24cf51a
Muzaffar Hussain <78912929+syedmuzaffarhussain1997@users.noreply.github.com>
2025-04-11 22:46:40 +0500

    Updated main.py file for executing on replit

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ def send_static(path):
 api_app.mount("/", WSGIMiddleware(flask_app))
 
 if __name__ == "__main__":
-    uvicorn.run(api_app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    uvicorn.run(api_app, host="0.0.0.0", port=port)
